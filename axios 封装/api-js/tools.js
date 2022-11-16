0f9068f1696da4c3da1f37c5e2978a4f41b59491
liupeng <dmc1253@163.com>
2022-11-16 23:49:33 +0800

    ✨ feat: 添加axios封装拦截设置示例

--- axios 封装/api-js/tools.js
+++ axios 封装/api-js/tools.js
@@ -1,7 +1,8 @@
 import { message } from "../antd";
 
 export const handleChangeRequestHeader = (config) => {
-  config["xxxx"] = "xxx";
+  // config["xxxx"] = "xxx";
+  // config["baseURL"] = process.env.VUE_APP_BASE_URL;
   return config;
 };
 
@@ -12,18 +13,18 @@ export const handleConfigureAuth = (config) => {
 
 export const handleNetworkError = (errStatus) => {
   const networkErrMap = {
-    "400": "错误的请求", // token 失效
-    "401": "未授权，请重新登录",
-    "403": "拒绝访问",
-    "404": "请求错误，未找到该资源",
-    "405": "请求方法未允许",
-    "408": "请求超时",
-    "500": "服务器端出错",
-    "501": "网络未实现",
-    "502": "网络错误",
-    "503": "服务不可用",
-    "504": "网络超时",
-    "505": "http版本不支持该请求",
+    400: "错误的请求", // token 失效
+    401: "未授权，请重新登录",
+    403: "拒绝访问",
+    404: "请求错误，未找到该资源",
+    405: "请求方法未允许",
+    408: "请求超时",
+    500: "服务器端出错",
+    501: "网络未实现",
+    502: "网络错误",
+    503: "服务不可用",
+    504: "网络超时",
+    505: "http版本不支持该请求",
   };
   if (errStatus) {
     message.error(networkErrMap[errStatus] ?? `其他连接错误 --${errStatus}`);
@@ -35,14 +36,14 @@ export const handleNetworkError = (errStatus) => {
 
 export const handleAuthError = (errno) => {
   const authErrMap = {
-    "10031": "登录失效，需要重新登录", // token 失效
-    "10032": "您太久没登录，请重新登录~", // token 过期
-    "10033": "账户未绑定角色，请联系管理员绑定角色",
-    "10034": "该用户未注册，请联系管理员注册用户",
-    "10035": "code 无法获取对应第三方平台用户",
-    "10036": "该账户未关联员工，请联系管理员做关联",
-    "10037": "账号已无效",
-    "10038": "账号未找到",
+    10031: "登录失效，需要重新登录", // token 失效
+    10032: "您太久没登录，请重新登录~", // token 过期
+    10033: "账户未绑定角色，请联系管理员绑定角色",
+    10034: "该用户未注册，请联系管理员注册用户",
+    10035: "code 无法获取对应第三方平台用户",
+    10036: "该账户未关联员工，请联系管理员做关联",
+    10037: "账号已无效",
+    10038: "账号未找到",
   };
 
   if (authErrMap.hasOwnProperty(errno)) {
