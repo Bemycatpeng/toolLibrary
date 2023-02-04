4913c95fe56854ec9df9a577ae4f855bb44d1da9
liupeng <dmc1253@163.com>
2023-02-04 14:37:31 +0800

    新增vue总线实现方式

---
+++ vue 总线 bus/子组件.vue
@@ -0,0 +1,22 @@
+// Child.vue
+
+<template>
+  <div>
+    子组件：
+    <button @click="handleBusEmit">触发Bus.$emit</button>
+    <button @click="changeBusMsg">修改总线里的 msg</button>
+  </div>
+</template>
+
+<script setup>
+import Bus from '../Bus.js'
+
+function handleBusEmit() {
+  Bus.$emit('changeMsg', '雷猴啊')
+}
+
+function changeBusMsg() {
+  // console.log(Bus.msg)
+  Bus.msg.value = '在子组件里修改了总线的值'
+}
+</script>
---
+++ vue 总线 bus/父组件.vue
@@ -0,0 +1,26 @@
+// Parent.vue
+
+<template>
+  <div>
+    父组件:
+    <span style="margin-right: 30px;">message: {{ message }}</span>
+    <span>msg: {{ msg }}</span>
+  </div>
+  <Child></Child>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import Bus from './Bus.js'
+import Child from './components/Child.vue'
+
+const msg = ref(Bus.msg)
+
+const message = ref('hello')
+
+// 用监听的写法
+Bus.$on('changeMsg', data => {
+  message.value = data
+})
+
+</script>
